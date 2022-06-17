b0d18066d58f08179ced021d2ca811fff9ee13e3
Derek Chancellor <me@chancellor.tech>
2022-06-17 12:31:23 -0500

    Init

---
+++ src/main/java/com/dynamicvoyage/thehunted/item/ModItems.java
@@ -0,0 +1,20 @@
+package com.dynamicvoyage.thehunted.item;
+
+import com.dynamicvoyage.thehunted.TheHunted;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TheHunted.MOD_ID);
+
+    public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TAB_THEHUNTED)));
+    public static final RegistryObject<Item> RAW_CITRINE = ITEMS.register("raw_citrine", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TAB_THEHUNTED)));
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
